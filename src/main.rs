ebb58762cd6ba9deb8fe984a4a7b55b294d9d0b5
lnwza007 ⚡️ <gcalku@gmail.com>
2024-12-19 13:09:31 +0700

    example to use

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use bitcoin_wallet::mnemonic::language::LANG;
 use bitcoin_wallet::mnemonic::BIP39;
 
 fn main() {
-    let i = BIP39::new(128, LANG::JP);
-    println!("{:?}", &i);
-    i.mnemonic()
-
+    BIP39::new(128, LANG::ENG).mnemonic();
+    BIP39::new(128, LANG::JP).mnemonic();
 }
